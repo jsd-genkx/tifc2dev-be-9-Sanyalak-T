1c0c3dd583439c78f3322b7bdebfbb20c3d3012d
Sanyalak T. <isanyalakt@gmail.com>
2025-04-21 15:57:20 +0700

    update(files): update index files follow guide documents

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
@@ -20,27 +21,67 @@ app.get("/books", (req, res, next) => {
   setTimeout(() => {
     const { genre } = req.query;
     //TODO: ADD CODE HERE ⬇️ to Filter books by genre.
-    const filteredBooks = books.filter((book) => book.genre.includes(genre));
+    try {
+      if(genre) {
+        const filteredBooks = books.filter((book) => book.genre.includes(genre));
+        res.send(filteredBooks);
+      } else {
+        res.send(books);
+      }
+    } catch(err) {
+      next(err);
+    }
+    // const filteredBooks = books.filter((book) => book.genre.includes(genre));
     //TODO: ADD CODE HERE ⬇️
   }, 1000); // Simulate a 1-second delay
 });
 
 // GET specific book by ID with async/await
 app.get("/books/:id", async (req, res, next) => {
-  const book = await new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const foundBook = books.find((b) => b.id === parseInt(req.params.id, 10));
-      if (foundBook) {
-        resolve(foundBook);
-      } else {
-        //TODO: ADD CODE to reject the promise
-      }
-    }, 1000); // Simulate a 1-second delay
-  });
-  //TODO: ADD CODE HERE ⬇️
+  try {
+    const book = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const foundBook = books.find((b) => b.id === parseInt(req.params.id, 10));
+        if (foundBook) {
+          resolve(foundBook);
+        } else {
+          //TODO: ADD CODE to reject the promise
+          reject(new Error("Book not found"));
+        }
+      }, 1000); // Simulate a 1-second delay
+    });
+    //TODO: ADD CODE HERE ⬇️
+    res.json(book);
+  }
+  //TODO: ADD CODE HERE
+  catch(err) {
+    err.status = 400;
+    next(err);
+  }
 });
 
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+})
+
 //TODO: ADD CODE HERE ⬇️
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const response = {
+      message: err.message || "Internal server error",
+      status: status,
+  }
+
+  if(process.env.NODE_ENV === "development") {
+      response.stack = err.stack;
+  }
+
+  console.error(err.stack);
+  res.status(status).json(response);
+
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
